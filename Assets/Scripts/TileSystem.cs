4b17ea3b277acb8efd697c5874fd05508c186b44
Jordan Muir <jordanhmuir@gmail.com>
2024-10-01 12:15:16 -0700

    PlaceableObjects now paint the TileMap where they are placed

--- Assets/Scripts/TileSystem.cs
+++ Assets/Scripts/TileSystem.cs
@@ -38,6 +38,27 @@ public class TileSystem : MonoBehaviour
         // {
         //     InitializeWithObject(prefab2);
         // }
+        if (!objectToPlace)
+        {
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (CanBePlaced(objectToPlace))
+            {
+                objectToPlace.Place();
+                Vector3Int start = gridLayout.WorldToCell(objectToPlace.GetStartPosition());
+                TakeArea(start, objectToPlace.Size);
+            }
+            else
+            {
+                Destroy(objectToPlace.gameObject);
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Destroy(objectToPlace.gameObject);
+        }
     }
     #endregion
 
@@ -62,6 +83,19 @@ public class TileSystem : MonoBehaviour
         position = grid.GetCellCenterWorld(cellPos);
         return position;
     }
+    private static TileBase[] GetTilesBlock(BoundsInt area, Tilemap tilemap)
+    {
+        TileBase[] array = new TileBase[area.size.x * area.size.y * area.size.z];
+        int counter = 0;
+
+        foreach(var v in area.allPositionsWithin)
+        {
+            Vector3Int pos = new Vector3Int(v.x,v.y,z:0);
+            array[counter] = tilemap.GetTile(pos);
+            counter++;
+        }
+        return array;
+    }
     #endregion 
     
     #region Plant Placement
@@ -76,6 +110,28 @@ public class TileSystem : MonoBehaviour
         Debug.Log("Added ObjectDrag component to: " + obj.name);
     }
 
+    private bool CanBePlaced(PlaceableObject placeableObject)
+    {
+        BoundsInt area = new BoundsInt();
+        area.position = gridLayout.WorldToCell(objectToPlace.GetStartPosition());
+        area.size = placeableObject.Size;
+
+        TileBase[] baseArray = GetTilesBlock(area, MainTilemap);
+        foreach(var b in baseArray)
+        {
+            if(b == whiteTile)
+            {
+                return false;
+            }
+            
+        }
+        return true;
+    }
+
+    public void TakeArea(Vector3Int start, Vector3Int size)
+    {
+        MainTilemap.BoxFill(start, whiteTile, start.x, start.y, start.x + size.x, start.y + size.y);
+    }
 
     #endregion 
 }
